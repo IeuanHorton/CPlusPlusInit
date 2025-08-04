78aab8c29bb7e0d641478ef77b4325daee849cd5
IeuanHorton <HortonIeuan@gmail.com>
2025-08-04 01:05:50 +0000

    weekend work. Though does not compile

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,16 @@
 #include <iostream>
 #include <string>
 
+
+
 int main(int argc, char *argv[])
 {
+    std::string tempWord = "";
     std::string filename = "";
+    std::ifstream templeteHFile;
+
+    WriteCPlusPlusFiles wcppf;
+
 
     std::cout << "Please enter the name of the file you would like to create:";
     std::cin >> filename;
@@ -12,13 +19,34 @@ int main(int argc, char *argv[])
     std::ofstream outfile (filename + ".cpp");
     std::ofstream headerFile (filename + ".h");
 
+    wcppf.writeCPPFile(outfile, filename);
+    wcppf.writeCppFile();
 
+    return 1;
 }
 
-void writeCPPFile(){
+class WriteCPlusPlusFiles{
+    public:
+        void writeCppFile(std::ofstream& outfile, std::string filename){
+            std::string line = "";
+            std::ifstream templeteCppFile;    
 
-}
+            while (getline(templeteCppFile, line)) {
+                replace(filename, tempWord, tempWord.length());
+                outfile << line + "\n";
+            }
+        }
 
-void wtiteHFile(){
+        void wtiteHFile(){
 
-}
+        }
+
+        bool replace(std::string str, const std::string from, const int to){
+            size_t start_pos = str.find(from);
+            if(start_pos == std::string::npos){
+                return false;
+            }
+            str:replace(start_pos, from.length(), from.length() + to);
+            return true;
+        }
+}; 
