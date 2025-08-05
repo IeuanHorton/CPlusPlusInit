fed2e66012b3b88d5161ab99f659bee4cebd5c1a
IeuanHorton <HortonIeuan@gmail.com>
2025-08-05 02:49:43 +0000

    bar work

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,6 @@
 #include <iostream>
 #include <string>
 
-
-
-int main(int argc, char *argv[])
-{
-    std::string tempWord = "";
-    std::string filename = "";
-    std::ifstream templeteHFile;
-
-    WriteCPlusPlusFiles wcppf;
-
-
-    std::cout << "Please enter the name of the file you would like to create:";
-    std::cin >> filename;
-
-    std::ofstream outfile (filename + ".cpp");
-    std::ofstream headerFile (filename + ".h");
-
-    wcppf.writeCPPFile(outfile, filename);
-    wcppf.writeCppFile();
-
-    return 1;
-}
-
 class WriteCPlusPlusFiles{
     public:
         void writeCppFile(std::ofstream& outfile, std::string filename){
@@ -46,7 +23,30 @@ class WriteCPlusPlusFiles{
             if(start_pos == std::string::npos){
                 return false;
             }
-            str:replace(start_pos, from.length(), from.length() + to);
+            std:replace(from, to);
             return true;
         }
+
+    private:
+        std::string tempWord = "";
 }; 
+
+int main(int argc, char *argv[])
+{
+    std::string filename = "";
+    std::ifstream templeteHFile;
+
+    WriteCPlusPlusFiles wcppf;
+
+
+    std::cout << "Please enter the name of the file you would like to create:";
+    std::cin >> filename;
+
+    std::ofstream outfile (filename + ".cpp");
+    std::ofstream headerFile (filename + ".h");
+
+    wcppf.writeCPPFile(outfile, filename);
+    wcppf.writeCppFile();
+
+    return 1;
+}
