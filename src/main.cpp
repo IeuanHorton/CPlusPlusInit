f3b1f71af8e16be532c422b53d42dc07cc0ecb5e
IeuanHorton <HortonIeuan@gmail.com>
2025-08-09 15:53:05 +0000

    cleand up and added comments

--- src/main.cpp
+++ src/main.cpp
@@ -9,22 +9,13 @@ class WriteCPlusPlusFiles{
             std::ifstream templeteCppFile;    
 
             while (getline(templeteCppFile, line)) {
-                replace(filename, tempWord, tempWord.length());
+
                 outfile << line + "\n";
             }
         }
 
-        void wtiteHFile(){
+        void writeHFile(){
 
-        }
-
-        bool replace(std::string str, const std::string from, const int to){
-            size_t start_pos = str.find(from);
-            if(start_pos == std::string::npos){
-                return false;
-            }
-            std:replace(from, to);
-            return true;
         }
 
     private:
@@ -38,15 +29,14 @@ int main(int argc, char *argv[])
 
     WriteCPlusPlusFiles wcppf;
 
-
     std::cout << "Please enter the name of the file you would like to create:";
-    std::cin >> filename;
+    std::cin >> filename;//Gets file name from the user
 
-    std::ofstream outfile (filename + ".cpp");
-    std::ofstream headerFile (filename + ".h");
+    std::ofstream outfile (filename + ".cpp");//Creates CPP file with the given name
+    std::ofstream headerFile (filename + ".h");//Creates Header file
 
-    wcppf.writeCPPFile(outfile, filename);
-    wcppf.writeCppFile();
+    wcppf.writeCppFile(outfile, filename);
+    wcppf.writeHFile();
 
     return 1;
 }
